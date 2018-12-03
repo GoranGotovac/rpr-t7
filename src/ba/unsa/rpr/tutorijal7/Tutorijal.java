b71c3535cb1a14fdf2e3d709ad71232c2712f693
GoranGotovac <ggotovac1@etf.unsa.ba>
2018-12-04 00:25:23 +0100

    Uradjena priprema

---
+++ src/ba/unsa/rpr/tutorijal7/Tutorijal.java
@@ -0,0 +1,66 @@
+package ba.unsa.rpr.tutorijal7;
+
+import org.w3c.dom.Document;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Tutorijal {
+
+    public static void main(String[] args) {
+
+
+        }
+    public static ArrayList<Grad> ucitajGradove(){
+    ArrayList<Grad> gradovi = new ArrayList<>();
+    Scanner ulaz = null;
+        try {
+            ulaz = new Scanner(new FileReader("mjerenja.txt.txt"));
+        } catch (FileNotFoundException e) {
+            System.out.println("Datoteka ulaz.txt ne postoji ili se ne može otvoriti");
+        }
+        try{
+            while(ulaz.hasNext()) {
+                String[] mjerenja = ulaz.nextLine().split(",");
+                Grad grad = new Grad();
+                grad.setNaziv(mjerenja[0]);
+                double[] niz;
+                niz = new double[mjerenja.length-1];
+                for(int i = 0; i<mjerenja.length; i++) {
+                    if(i==1000) break;
+                    niz[i] = Double.parseDouble(mjerenja[i+1]);
+                }
+                grad.setTemperature(niz);
+                gradovi.add(grad);
+            }
+         }
+         catch(Exception e) {
+             System.out.println(e.getMessage());
+         }
+         finally {
+            try{
+                ulaz.close();
+            }
+            catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        return gradovi;
+    }
+    public static UN ucitajXml(ArrayList<Grad> gradovi) {
+        UN un = new UN();
+        Document xmldoc = null;
+        try {
+            DocumentBuilder docReader = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+            xmldoc = docReader.parse(new File("drzave.xml"));
+        } catch (Exception e) {
+            System.out.println("drzave.xml nije validan XML dokument");
+        }
+    return un;
+    }
+}
---
+++ src/ba/unsa/rpr/tutorijal7/UN.java
@@ -0,0 +1,20 @@
+package ba.unsa.rpr.tutorijal7;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UN {
+    private ArrayList<Drzava> drzave = new ArrayList<>();
+
+    public UN() {
+    }
+
+    public List<Drzava> getDrzave() {
+        return drzave;
+    }
+
+    public void setDrzave(ArrayList<Drzava> drzave) {
+        this.drzave = drzave;
+    }
+}
+
